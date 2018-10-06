d584296b9bffd74e41cea8a464cc0e33af30c811
LLlon57135 <43909690+LLlon57135@users.noreply.github.com>
2018-10-06 17:31:50 +0300

    initial commit

---
+++ babyfarmers/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1538836217,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/hero-sheet0.png",
+		"images/grass.png",
+		"images/plant-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/invcase-sheet0.png",
+		"images/itemdrop-sheet0.png",
+		"images/itemdrop-sheet1.png",
+		"images/itemui-sheet0.png",
+		"images/countinv.png",
+		"images/chest-sheet0.png",
+		"images/chest-sheet1.png",
+		"images/chestfon-sheet0.png",
+		"images/exitchest-sheet0.png",
+		"images/menutile.png",
+		"images/menubutton-sheet0.png",
+		"images/bed-sheet0.png",
+		"images/planting-sheet0.png",
+		"images/planting-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
